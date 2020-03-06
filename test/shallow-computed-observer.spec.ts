3d4913839ee1caf110092e9f9f5024c1e6dcc28d
bigopon.777@gmail.com <bigopon.777@gmail.com>
2020-03-06 22:00:13 +1100

    chore: add tests for shallow computed decorator

---
+++ test/shallow-computed-observer.spec.ts
@@ -0,0 +1,78 @@
+import './setup';
+import { deepComputedFrom, shallowComputedFrom } from "../src";
+import { bootstrapComponent } from "./utils";
+
+fdescribe('shallow-computed-observer.spec.ts', () => {
+
+  interface IModel {
+    coins: {
+      $1: number;
+      $2: number;
+    };
+    notes: {
+      $5: number;
+      $10: number;
+    }
+  }
+
+  const createModel = (): IModel => {
+    return {
+      coins: {
+        $1: 1,
+        $2: 1,
+      },
+      notes: {
+        $5: 1,
+        $10: 1
+      }
+    };
+  };
+
+  it('works with getter', async () => {
+    class App {
+      static $view = '<template>${json}</template>';
+
+      data = createModel();
+
+      jsonCallCount = 0;
+
+      @shallowComputedFrom('data')
+      get json() {
+        this.jsonCallCount++;
+        return JSON.stringify(this.data);
+      }
+    }
+
+    const { viewModel, host, taskQueue } = await bootstrapComponent(App);
+    expect(host.textContent).toBe(JSON.stringify(viewModel.data));
+    // there are 2 access calls to the getter
+    // 1 is from initial value evaluation, and another is for when setting up the observer
+    expect(viewModel.jsonCallCount).toBe(2);
+
+    // shallow observation won't observe deeply into coins/notes objects
+    viewModel.data.coins.$1++;
+    taskQueue.flushMicroTaskQueue();
+    expect(viewModel.jsonCallCount).toBe(2);
+    expect(host.textContent).toBe(JSON.stringify(createModel()));
+
+    // now test replacing the root property
+    const newModel = createModel();
+    newModel.coins.$1 = 5;
+    viewModel.data.coins = newModel.coins;
+    taskQueue.flushMicroTaskQueue();
+    expect(viewModel.jsonCallCount).toBe(4);
+    expect(host.textContent).toBe(JSON.stringify(newModel));
+  });
+
+  describe('array test caces', function todo() {
+
+  });
+
+  describe('set test cases', function todo() {
+
+  });
+
+  describe('map test cases', function todo() {
+
+  });
+});
